6390aec6458882d39253c3e1bdb5f0e01266d265
Cash <32471210+pickfr@users.noreply.github.com>
2023-12-21 03:43:45 +0000

    Adds win conditions working for rows and columns
    No diagonal yet, so that needs adding and possible bug caused by
    changes to the getPlayer() function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gameboard = (function () {
+const gameboard = () => {
 
     const columns = 3;
     const rows = 3;
@@ -14,7 +14,40 @@ const gameboard = (function () {
 
     const getBoard = () => board;
 
-    const dropToken = (x, y, playerToken) => board[x][y].addToken(playerToken);
+    const dropToken = (x, y, playerToken) => {
+        if (board[x][y].getValue() === 0) {
+            board[x][y].addToken(playerToken);
+            console.log(`Adding ${playerController().getCurrentPlayer().name}'s marker into square: ${x}, ${y}`);
+            checkWinner(x,y,playerToken);
+            return true;
+        } else {
+            console.log("Token already placed here. Please choose another space.")
+            printBoard();
+            return false;
+        }
+    }
+
+    const checkWinner = (x, y, checkForPlayer) => {
+
+        //check rows
+        for (i = 0; i < rows; i++) {
+            if(board[x][i].getValue() !== checkForPlayer) break;
+            if(i === rows-1){
+                //Report Win and do scorey stuff
+                console.log("Winner found on the horizontal")
+            }
+        }
+
+        //check column
+        for (i = 0; i < columns; i++) {
+            if(board[i][y].getValue() !== checkForPlayer) break;
+            if(i === columns-1){
+                //Report Win and do scorey stuff
+                console.log("Winner found on the vertical")
+            }
+        }
+    }
+
 
     // Temporary board renderer
     const printBoard = () => {
@@ -22,9 +55,9 @@ const gameboard = (function () {
         console.log(extractedBoardValues);
     }
 
-    return { getBoard, dropToken, printBoard};
+    return { getBoard, dropToken, printBoard };
 
-}());
+};
 
 function cell() {
 
@@ -37,3 +70,50 @@ function cell() {
     return { getValue, addToken }
 
 }
+
+function playerController(
+    playerOneName = "Player One",
+    playerTwoName = "Player Two"
+) {
+
+    const board = gameboard();
+
+
+    players = [
+        {
+            name: playerOneName,
+            token: 1
+        },
+        {
+            name: playerTwoName,
+            token: 2
+        }
+    ]
+
+
+    let currentPlayer = players[0];
+
+    const switchActivePlayer = () => {
+        currentPlayer = currentPlayer === players[0] ? players[1] : players[0]
+    }
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    const playRound = (x, y) => {
+
+        if (board.dropToken(x, y, getCurrentPlayer().token)) {
+
+            board.printBoard();
+            switchActivePlayer();
+        }
+
+    }
+
+
+    return { getCurrentPlayer, playRound }
+
+
+
+}
+
+game = playerController();
